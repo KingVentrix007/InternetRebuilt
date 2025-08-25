0e672e4c6d28720a1e1c6345e54091305f08f093
Tristan Kuhn <tristanjkuhn007@gmail.com>
2025-08-25 05:50:28 +0200

    DNS version 1

--- DNS/table.py
+++ DNS/table.py
@@ -1,5 +1,33 @@
 
 master_table = {}
-def add_dns_entry(url,ip):
+def add_dns_entry(url,ip:list[str]):
     broken_url = url.split(".")[::-1]
-    
+    current_table = master_table
+    for part in broken_url:
+        next_table = current_table.get(part,None)
+        if(next_table == None):
+            current_table[part] = {}
+            next_table = current_table.get(part,None)
+        current_table = next_table
+    if(current_table.get("ip",None) == None):
+        current_table["ip"] = ip
+    else:
+        for i in ip:
+            current_table["ip"].append(i)
+
+def search(url):
+    broken_url = url.split(".")[::-1]
+    current_table = master_table
+    for part in broken_url:
+        next_table = current_table.get(part,None)
+        if(next_table == None):
+            return None
+        current_table = next_table
+    return current_table["ip"]
+def test():
+    add_dns_entry("example.co.za",["1.1.1.1"])
+    add_dns_entry("example.za",["3.2.1.0"])
+
+    print(search("example.za"))
+
+test()
